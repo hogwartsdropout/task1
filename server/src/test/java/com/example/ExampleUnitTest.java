6426358757b419d5d8280f236bad7675fb1fd961
Dmitry Shanin <dmitriy.shanin@mera.com>
2020-01-28 21:19:42 +0300

    test

---
+++ server/src/test/java/com/example/ExampleUnitTest.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+//Not much to test here. TODO
+public class ExampleUnitTest {
+    @Test
+    public void addition_test() {
+        assertEquals(3, 1 + 2);
+    }
+
+    @Test
+    public void orwellianTruth() {
+        assertEquals(5, 2 + 2);
+    }
+}
